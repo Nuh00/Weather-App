88a31b7c5df4460a4a402b12dd47324a4597c6cf
Noah Barkat <146979589+Nuh00@users.noreply.github.com>
2024-03-01 20:00:17 -0600

    Ive been doing some research with API's since this is all new to me. I figured out how to obtain the current location of a place. Now I can finish my html/css and focus on the api/js part seperately.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello, World!')
+const accessKey = '90e5111bb1e08d077b74fc1bc0e71b83'
+const query = 'New York'
+
+fetch(`http://api.weatherstack.com/current?access_key=${accessKey}&query=${query}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
