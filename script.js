ca41699f31f42354c1fe599867fd102e26dd9c40
Noah Barkat <146979589+Nuh00@users.noreply.github.com>
2024-03-03 18:42:25 -0600

    I want to change my look of my website but right now I just worked on obtaining the info inside of the inptu field then created a function called getWeather that grabs the value and fetches it from the api

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-const accessKey = '90e5111bb1e08d077b74fc1bc0e71b83'
-const query = 'New York'
 
-fetch(`http://api.weatherstack.com/current?access_key=${accessKey}&query=${query}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
+const city = document.getElementById('city')
+
+
+const accessKey = 'e010a098fe24fdbb489eb5b1bfe1451f'
+
+// const query = city.value
+
+// fetch(`http://api.weatherstack.com/current?access_key=${accessKey}&query=${query}`)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//     })
+
+
+function getWeather() {
+    const query = city.value
+    fetch(`http://api.weatherstack.com/current?access_key=${accessKey}&query=${query}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+
+getWeather()
